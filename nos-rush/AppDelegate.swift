fc111c2eb5bf96c908ac7ecae33c8e81c7d4e379
LesleyMerks <lesleymerks@gmail.com>
2014-12-11 13:49:09 +0100

    commit

--- nos-rush/AppDelegate.swift
+++ nos-rush/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+      
+        var navigationBarAppearace = UINavigationBar.appearance()
+        
+        
+        
+        navigationBarAppearace.tintColor = UIColor.clearColor()
+        navigationBarAppearace.barTintColor = UIColor.clearColor()
+        
+
+            
         return true
     }
 
@@ -40,6 +50,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+  
 
 
 }
---
+++ nos-rush/customTabController.swift
@@ -0,0 +1,27 @@
+//
+//  customTabController.swift
+//  nos-rush
+//
+//  Created by Lesley Merks on 28/11/14.
+//  Copyright (c) 2014 zoetzuur. All rights reserved.
+//
+
+import UIKit
+
+class customTabController: UITabBarController  {
+    
+    
+       override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // load second tabbarview
+       self.selectedIndex = 1;
+
+       
+            
+            
+    }
+   
+    
+
+}
